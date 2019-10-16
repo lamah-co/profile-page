cfc836f19d48c4a8b70ca8db024120dd5819e9cc
AliAqoub <ali@lamah.com>
2019-10-16 14:28:57 +0200

    New Project

---
+++ index.php
@@ -0,0 +1,200 @@
+<!doctype html>
+<html lang="ar">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="css/custom.css" type="text/css">
+  <link rel="stylesheet" href="js/jquery.slim.min.js" type="text/css">
+  <script src="https://kit.fontawesome.com/78cade43c4.js" crossorigin="anonymous"></script>
+  <title>Welcome To Lamah</title>
+</head>
+<body>
+  <!-- Page Content for PC and Tablet -->
+  <div class="container d-sm-none d-md-block d-md-none d-lg-block d-none d-sm-block">
+    <div class="shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="card-body p-4">
+        <div style="height: auto;">
+          <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+              <div class="card-deck">
+                <div class="card">
+                  <img class="card-img-top" src="images/profile.png" alt="profile photo">
+                  <div class="card-body col-sm-12 col-12">
+                    <h5 class="card-title">بيانات شخصية</h5>
+                    <ul class="list-group list-group-flush" style="direction:rtl !important;">
+                      <li class="list-group-item"><i class="far fa-paper-plane">&nbsp&nbsp&nbsp</i>email@lamah.com</li>
+                      <li class="list-group-item"><i class="fas fa-mobile-alt">&nbsp&nbsp&nbsp</i>091 000 00 00</li>
+                      <li class="list-group-item"><i class="far fa-calendar-alt">&nbsp&nbsp&nbsp</i>01/01/1999</li>
+                      <li class="list-group-item"><i class="fas fa-burn">&nbsp&nbsp&nbsp</i>A+</li>
+                      <li class="list-group-item"><i class="fas fa-dollar-sign">&nbsp&nbsp&nbsp</i>35 د.ل</li>
+                    </ul>
+                  </div>
+                  <div class="card-footer">
+                    <a href="#" class="btn btn-outline-info">تسجيل خروج</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+              <p class="h2">مرحباً ...</p>
+              <hr>
+              <br>
+              <p class="h4">بيانات الاتصال</p>
+              <div class="row cont justify-content-md-center">
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="circle-gr">
+                    <h6>التحميل</h6>
+                    28.5 MiB
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="circle-rd">
+                    <h6>الرفع</h6>
+                    1316.4 MiB
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="circle-bl">
+                    <h6>زمن الاتصال</h6>
+                    2h4m8s
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="circle-g">
+                    <h6>تحديث الحالة</h6>
+                    1 m
+                  </div>
+                </div>
+              </div>
+              <p class="h4 cont">خدمات</p>
+              <hr width=30%; align=right !important;>
+              <div class="row serv">
+                <div class="col-4">
+                  <img class="icon-img" src="images/coff.png" alt="..." class="rounded" data-toggle="modal"  data-target=".bd-example-modal-lg">
+                </div>
+                <div class="col-4">
+                  <img class="icon-img" src="images/inter.png" alt="..." class="rounded">
+                </div>
+                <div class="col-4">
+                  <img class="icon-img" src="images/support.png" alt="..." class="rounded">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--End of Page Content for PC and Tablet -->
+
+  <!-- Page Content for Mobile -->
+  <div class="container d-block d-sm-none d-none d-sm-block d-md-none">
+    <div class="card" style="width: 100%; margin-top:2%;">
+      <img class="card-img-top" src="images/profile.png" alt="Card image cap" style="margin-right:25%;">
+      <div class="card-body">
+        <h5 class="card-title">مرحبا ..</h5>
+        <p class="card-text"></p>
+        <div class="accordion" id="accordionExample">
+          <div class="card">
+            <div class="card-header" id="headingOne"  style="background-color:#747d8c;">
+              <h5 class="mb-0">
+                <button style="text-decoration: none !important; color:#fff !important;" class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  بيانات شخصية
+                </button>
+              </h5>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                <ul class="list-group list-group-flush" style="direction:rtl !important;">
+                  <li class="list-group-item"><i class="far fa-paper-plane">&nbsp&nbsp&nbsp</i>email@lamah.com</li>
+                  <li class="list-group-item"><i class="fas fa-mobile-alt">&nbsp&nbsp&nbsp</i>091 000 00 00</li>
+                  <li class="list-group-item"><i class="far fa-calendar-alt">&nbsp&nbsp&nbsp</i>01/01/1999</li>
+                  <li class="list-group-item"><i class="fas fa-burn">&nbsp&nbsp&nbsp</i>A+</li>
+                  <li class="list-group-item"><i class="fas fa-dollar-sign">&nbsp&nbsp&nbsp</i>35 د.ل</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo" style="background-color:#a4b0be;">
+              <h5 class="mb-0">
+                <button style="text-decoration: none !important; color:#fff !important;" class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  بيانات الاتصال
+                </button>
+              </h5>
+            </div>
+            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div class="card-body">
+                <table class="table table-sm">
+                  <thead>
+                    <tr>
+                      <th scope="col">تحديث الحالة</th>
+                      <th scope="col">22h38m45s</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>التحميل</td>
+                      <td>1164.3 MiB</td>
+                    </tr>
+                    <tr>
+                      <td>الرفع</td>
+                      <td>33.9 MiB</td>
+                    </tr>
+                    <tr>
+                      <td>زمن الاتصال</td>
+                      <td>22h38m45s</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row" style="margin-right:4%; margin-top:10%;">
+          <div class="col-4">
+            <img class="icon-mob" src="images/coff.png" alt="..." class="rounded" data-toggle="modal"  data-target=".bd-example-modal-lg">
+            <span class="icon_tit">المقهى</span>
+          </div>
+          <div class="col-4">
+            <img class="icon-mob" src="images/inter.png" alt="..." class="rounded">
+            <span class="icon_tit" style="margin-right:0.2em;">الباقات</span>
+          </div>
+          <div class="col-4">
+            <img class="icon-mob" src="images/support.png" alt="..." class="rounded">
+            <span class="icon_tit" style="margin-right:0.5em;">أخرى</span>
+          </div>
+        </div>
+        <br>
+        <a href="#" class="btn btn-outline-info" style="margin-right:31%;">تسجيل خروج</a>
+      </div>
+    </div>
+  </div>
+  <!--End of Page Content for mobile -->
+
+  <!-- coffee popup -->
+  <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <h5 class="card-title">المقهى</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end coffee popup -->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
